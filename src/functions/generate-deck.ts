9e44a46bc78e996b69c98f8ac870671a08a5749c
Luke Davis <dowhileluke@gmail.com>
2023-10-06 10:19:54 -0700

    optional param

--- src/functions/generate-deck.ts
+++ src/functions/generate-deck.ts
@@ -1,5 +1,6 @@
-import { generateArray } from '@dowhileluke/fns';
+import { generateArray } from '@dowhileluke/fns'
+import { CardId } from '../types'
 
-export function generateDeck(suitCount: number) {
-	return generateArray(52, n => n % (13 * suitCount))
+export function generateDeck(suitCount = 4) {
+	return generateArray(52, (n): CardId => n % (13 * suitCount))
 }
