2bf94d00297c38ec5ec1541a19a78cbe6da5dcc9
Luke Davis <dowhileluke@gmail.com>
2024-01-02 12:27:27 -0800

    export aces

--- src-v3/functions/to-plaintext.ts
+++ src-v3/functions/to-plaintext.ts
@@ -3,10 +3,15 @@ import { CARD_DATA } from '../data'
 import { GameState } from '../types'
 
 export function toPlaintext(state: GameState) {
-	const fcText = state.cells.filter(x => x !== null).map(id => CARD_DATA[id!].initials).join(' ')
+	const cellsText = state.cells.filter(x => x !== null).map(id => CARD_DATA[id!].initials).join(' ')
+	const acesText = state.foundations.filter(x => x !== null).map(id => CARD_DATA[id!].initials).join(' ')
+	const pilesText = state.tableau.map(t => t.cardIds.map(id => CARD_DATA[id].initials).join(' '))
+
+	if (acesText) pilesText[0] += ` ${acesText}`
+
 	const result = truthy([
-		fcText && `FC: ${fcText}`,
-		state.tableau.map(t => t.cardIds.map(id => CARD_DATA[id].initials).join(' ')).join('\n')
+		cellsText && `FC: ${cellsText}`,
+		pilesText.join('\n')
 	]).join('\n').toUpperCase()
 
 	return result
