948c4e5f184fc8eb3d9413e4fb9c70528c64ba89
Luke Davis <dowhileluke@gmail.com>
2024-02-13 16:57:17 -0800

    begin tips

---
+++ src-v3/tips.ts
@@ -0,0 +1,54 @@
+// import { GameKey } from './games2'
+
+// endeavor...
+
+type Tip = string | string[]
+type GameTips = Tip[]
+
+const REVEAL = 'Revealing all face-down cards is essential to victory.'
+const INTACT = 'Leave the final card of each pile intact unless a King can immediately fill the space.'
+
+// klondike
+const klondike: GameTips = [
+	REVEAL,
+	'Prioritize moving cards within the tableau instead of making moves from the stock.',
+	INTACT,
+]
+
+
+// westcliff
+// see klondike
+
+// easthaven
+'Empty spaces are incredibly helpful for '
+
+// whitehead
+// klondike2
+// easthaven2
+// irmgard
+// spider
+// spiderette
+// will
+// simple
+
+const DOUBLE = 'Prioritize '
+
+// freecell
+'Look for moves that '
+// bakers
+// seahaven
+// forecell
+// yukon
+// russian
+// scorpion
+// wasp
+// yukon2
+// beleaguered
+// fortress
+// alleys
+// stronghold
+// canister
+// thieves
+// eight
+// alibaba
+'Until a space is open, no strategy is necessary. It is objectively correct to make every available move.'
