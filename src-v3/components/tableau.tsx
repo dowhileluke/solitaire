34e3f3fdacfbaf595af7dfa59d364c0f4f3a50d0
Luke Davis <dowhileluke@gmail.com>
2024-11-11 23:29:16 -0800

    split tableau

--- src-v3/components/tableau.tsx
+++ src-v3/components/tableau.tsx
@@ -4,15 +4,17 @@ import { Foundations } from './foundations'
 import { PileGroup } from './pile-group'
 import { Pile } from './pile'
 import classes from './tableau.module.css'
+import { concat } from '../functions/concat'
 
 export function Tableau() {
 	const [{ config, history, layoutMode }] = useAppState()
 	const emptyNode = config.emptyRestriction === 'kings' ? 'K' : null
 	const { tableau } = tail(history)
+	const isMassive = tableau.length > 10
 
-	if (layoutMode === 'horizontal') {
+	if (layoutMode === 'horizontal' && !isMassive) {
 		return (
-			<PileGroup className={classes.tableau}>
+			<PileGroup>
 				{tableau.map((pile, x) => (
 					<Pile
 						key={x}
@@ -29,26 +31,26 @@ export function Tableau() {
 	const [left, right] = split(tableau, half)
 
 	return (
-		<div className={classes.multi}>
-			<PileGroup vertical>
+		<div className={concat(classes.multi, isMassive && classes.vertical)}>
+			<PileGroup vertical={!isMassive}>
 				{left.map((pile, x) => (
 					<Pile
 						key={x}
 						toPos={y => ({ zone: 'tableau', x, y })}
 						emptyNode={emptyNode}
-						angle='W'
+						angle={isMassive ? 'S' : 'W'}
 						{...pile}
 					/>
 				))}
 			</PileGroup>
 			<Foundations groupIndex={0} vertical />
-			<PileGroup vertical>
+			<PileGroup vertical={!isMassive}>
 				{right.map((pile, i) => (
 					<Pile
 						key={i}
 						toPos={y => ({ zone: 'tableau', x: half + i, y })}
 						emptyNode={emptyNode}
-						angle='E'
+						angle={isMassive ? 'S' : 'E'}
 						{...pile}
 					/>
 				))}
