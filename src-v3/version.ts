6dd7266a3c2f0712e23530a845a4412c53f52c02
Luke Davis <dowhileluke@gmail.com>
2026-08-01 14:30:20 -0700

    Nudge horizontal faces

--- src-v3/version.ts
+++ src-v3/version.ts
@@ -6,6 +6,7 @@ type Version = {
 
 const VERSION_2: Version = { v: '2', name: 'Version 2', isMajor: true, }
 const VERSION_HISTORY: Version[] = [
+	{ v: '2.5.2', name: 'Nudge horizontal faces', },
 	{ v: '2.5.1', name: 'Adjust face card url', },
 	{ v: '2.5', name: 'Face cards', isMajor: true, },
 	{ v: '2.8.4', name: 'Theming tweaks', },
