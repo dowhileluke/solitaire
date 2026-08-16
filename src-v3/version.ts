daf430e9921a1750aa7ac2e6f747e0ae45059cd5
Luke Davis <dowhileluke@gmail.com>
2026-08-15 17:40:02 -0700

    forestgreen 110%

--- src-v3/version.ts
+++ src-v3/version.ts
@@ -6,6 +6,7 @@ type Version = {
 
 const VERSION_2: Version = { v: '2', name: 'Version 2', isMajor: true, }
 const VERSION_HISTORY: Version[] = [
+	{ v: '2.6.5', name: 'forestgreen 110%', },
 	{ v: '2.6.4', name: 'forestgreen', },
 	{ v: '2.6.3', name: 'Vivid face filters', },
 	{ v: '2.6.2', name: 'Improved face filters', },
